5d432a5c6319b3717edcff6fd3ea387a906a4373
tmccf <themmotcoccafe@gmail.com>
2023-01-02 00:11:44 +0700

    update: update git config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,8 +17,9 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'themmotcoccafe', // Usually your GitHub org/user name.
-  projectName: 'tmccf-docs', // Usually your repo name.
-
+  projectName: 'themmotcoccafe.github.io', // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
@@ -36,15 +37,15 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/styles/custom.scss'),
@@ -79,7 +80,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/themmotcoccafe/themmotcoccafe.github.io',
             label: 'GitHub',
             position: 'right',
           },
